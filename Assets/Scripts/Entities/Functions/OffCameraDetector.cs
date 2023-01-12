e786376e8c4f870e2982b7ebaf36f8bfd97e111c
Maxim Moskalyk <ictic.step@gmail.com>
2023-01-12 14:50:15 +0200

    Fixed camera missing reference exception on finishing game in Unity Editor.

--- Assets/Scripts/Entities/Functions/OffCameraDetector.cs
+++ Assets/Scripts/Entities/Functions/OffCameraDetector.cs
@@ -17,9 +17,30 @@ namespace Entities.Functions
 
         private void OnBecameInvisible()
         {
-            if(_camera is null)
+            if (!HasAccessToCamera()) 
                 return;
-            
+
+            SignalOffCamera();
+        }
+
+        private bool HasAccessToCamera()
+        {
+            try
+            {
+                // ReSharper disable once Unity.NoNullPropagation
+                if (_camera?.gameObject is null)
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SignalOffCamera()
+        {
             var position = transform.position;
             var bounds = _camera.GetVisibleBounds();
 
