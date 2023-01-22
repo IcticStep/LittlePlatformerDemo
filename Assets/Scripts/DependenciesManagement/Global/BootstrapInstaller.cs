d8450ef98f69affb48f4fb1c4bc986573e8c4cf6
Maxim Moskalyk <ictic.step@gmail.com>
2023-01-22 11:20:37 +0200

    Camera and Event System moved into ProjectContext Prefab; dissapearing of UI and player bug has been added.

--- Assets/Scripts/DependenciesManagement/Global/BootstrapInstaller.cs
+++ Assets/Scripts/DependenciesManagement/Global/BootstrapInstaller.cs
@@ -2,40 +2,32 @@ using Entities.Controls;
 using Entities.Functions;
 using Entities.System;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using Zenject;
 
 namespace DependenciesManagement.Global
 {
     public class BootstrapInstaller : MonoInstaller
     {
+        [Header("Children")]
+        [SerializeField] private Camera _cameraChild;
+        
+        [Header("Prefabs")]
         [SerializeField] private Player _playerPrefab;
-        [SerializeField] private Camera _mainCameraPrefab;
         [SerializeField] private LevelSwitcher _levelSwitcherPrefab;
-        [SerializeField] private EventSystem _eventSystemPrefab;
 
         // ReSharper disable Unity.PerformanceAnalysis
         public override void InstallBindings()
         {
-            CreateEventSystem();
-            
             BindCamera();
             BindLevelSwitcher();
             BindPlayer();
         }
 
-        private void CreateEventSystem()
-        {
-            var eventSystem = Container.InstantiatePrefabForComponent<EventSystem>(_eventSystemPrefab);
-            DontDestroyOnLoad(eventSystem);
-        }
-
         private void BindCamera()
         {
-            var camera = Container.InstantiatePrefabForComponent<Camera>(_mainCameraPrefab, new Vector3(0,0,-10), default, null);
             Container
                 .Bind<Camera>()
-                .FromInstance(camera)
+                .FromInstance(_cameraChild)
                 .AsSingle();
         }
 
